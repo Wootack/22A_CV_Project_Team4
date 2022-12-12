bf3d65e89dd2694c335b4fdd586ebfc5fc7bf4d2
Wootack <kwtsol@snu.ac.kr>
2022-12-12 10:06:20 +0900

    Keeper neglected & bb box size corrected.

--- save_video/save_video.py
+++ save_video/save_video.py
@@ -32,7 +32,7 @@ def save_video(video, startFrame, ball_xywh_array, red_tlwhs_array, blue_tlwhs_a
         # cv2.rectangle(frame, ball_lu, ball_rd, BALL_COLOR, 2)
         for rtid, fr_xlwh in enumerate(red_tlwhs_array):
             rtid_lu = fr_xlwh[fr, :2]
-            rtid_rd = rtid_lu + fr_xlwh[fr, 3:]
+            rtid_rd = rtid_lu + fr_xlwh[fr, 2:]
             if fr < startFrame:
                 cv2.rectangle(frame, tuple(rtid_lu), tuple(rtid_rd), RED_COLOR, 2)
                 cv2.putText(frame, str(rtid), tuple(rtid_lu),
@@ -52,7 +52,7 @@ def save_video(video, startFrame, ball_xywh_array, red_tlwhs_array, blue_tlwhs_a
                         # cv2.FONT_HERSHEY_PLAIN, 2, RED_COLOR, 2)
         for btid, fr_xlwh in enumerate(blue_tlwhs_array):
             btid_lu = fr_xlwh[fr, :2]
-            btid_rd = btid_lu + fr_xlwh[fr, 3:]
+            btid_rd = btid_lu + fr_xlwh[fr, 2:]
             if fr < startFrame:
                 cv2.rectangle(frame, tuple(btid_lu), tuple(btid_rd), BLUE_COLOR, 2)
                 cv2.putText(frame, str(btid), tuple(btid_lu),
@@ -96,7 +96,7 @@ def save_video_basic(video, ball_xywh_array, red_tlwhs_array, blue_tlwhs_array, 
         # cv2.rectangle(frame, ball_lu, ball_rd, BALL_COLOR, 2)
         for rtid, fr_xlwh in enumerate(red_tlwhs_array):
             rtid_lu = fr_xlwh[fr, :2]
-            rtid_rd = rtid_lu + fr_xlwh[fr, 3:]
+            rtid_rd = rtid_lu + fr_xlwh[fr, 2:]
             cv2.rectangle(frame, tuple(rtid_lu), tuple(rtid_rd), RED_COLOR, 2)
             # cv2.rectangle(frame, rtid_lu, rtid_rd, RED_COLOR, 2)
             cv2.putText(frame, str(rtid), tuple(rtid_lu),
@@ -105,7 +105,7 @@ def save_video_basic(video, ball_xywh_array, red_tlwhs_array, blue_tlwhs_array, 
                     # cv2.FONT_HERSHEY_PLAIN, 2, RED_COLOR, 2)
         for btid, fr_xlwh in enumerate(blue_tlwhs_array):
             btid_lu = fr_xlwh[fr, :2]
-            btid_rd = btid_lu + fr_xlwh[fr, 3:]
+            btid_rd = btid_lu + fr_xlwh[fr, 2:]
             cv2.rectangle(frame, tuple(btid_lu), tuple(btid_rd), BLUE_COLOR, 2)
             # cv2.rectangle(frame, btid_lu, btid_rd, BLUE_COLOR, 2)
             cv2.putText(frame, str(btid), tuple(btid_lu),
